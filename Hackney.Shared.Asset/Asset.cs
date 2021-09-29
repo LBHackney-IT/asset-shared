7a6d89e2c20d76dae1cf46daa87def016b8bb316
Alex Demetriou <alex.demetriou@hackney.gov.uk>
2021-09-29 12:08:17 +0100

    Add Create method
    Also, add IsAssetCautionaryAlerted boolean

--- Hackney.Shared.Asset/Asset.cs
+++ Hackney.Shared.Asset/Asset.cs
@@ -9,6 +9,7 @@ namespace Hackney.Shared.Asset
         public AssetType AssetType { get; set; }
         public string RootAsset { get; set; }
         public string ParentAssetIds { get; set; }
+        public bool IsAssetCautionaryAlerted { get; set; }
 
         public AssetLocation AssetLocation { get; set; }
         public AssetAddress AssetAddress { get; set; }
@@ -17,5 +18,23 @@ namespace Hackney.Shared.Asset
         public AssetTenure Tenure { get; set; }
 
         public int? VersionNumber { get; set; }
+
+        public static Asset Create(string id,
+            string assetId,
+            string assetType,
+            bool isAssetCautionaryAlerted,
+            AssetAddress assetAddress,
+            AssetTenure tenure)
+        {
+            return new Asset
+            {
+                Id = Guid.Parse(id),
+                AssetId = assetId,
+                AssetType = (AssetType)Enum.Parse(typeof(AssetType), assetType),
+                IsAssetCautionaryAlerted = isAssetCautionaryAlerted,
+                AssetAddress = assetAddress,
+                Tenure = tenure
+            };
+        }
     }
 }
