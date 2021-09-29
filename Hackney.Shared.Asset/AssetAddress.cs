1cf109404af1e2bdf74f6ab85b697ecec8def256
Alex Demetriou <alex.demetriou@hackney.gov.uk>
2021-09-29 14:52:48 +0100

    Add Create method for AssetAddress

--- Hackney.Shared.Asset/AssetAddress.cs
+++ Hackney.Shared.Asset/AssetAddress.cs
@@ -9,5 +9,25 @@ namespace Hackney.Shared.Asset
         public string AddressLine4 { get; set; }
         public string PostCode { get; set; }
         public string PostPreamble { get; set; }
+
+        public static AssetAddress Create(string uprn,
+            string addressLine1,
+            string addressLine2,
+            string addressLine3,
+            string addressLine4,
+            string postCode,
+            string postPreamble)
+        {
+            return new AssetAddress
+            {
+                Uprn = uprn,
+                AddressLine1 = addressLine1,
+                AddressLine2 = addressLine2,
+                AddressLine3 = addressLine3,
+                AddressLine4 = addressLine4,
+                PostCode = postCode,
+                PostPreamble = postPreamble
+            };
+        }
     }
 }
