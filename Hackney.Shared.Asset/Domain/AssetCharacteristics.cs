4e60ae17be94921c0b6736ca26a3cba1eb085b7d
venkatrangala <venkatrangala@gmail.com>
2022-04-14 11:31:00 +0100

    New fields for Temporary accommodation

--- Hackney.Shared.Asset/Domain/AssetCharacteristics.cs
+++ Hackney.Shared.Asset/Domain/AssetCharacteristics.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Hackney.Shared.Asset.Domain
 {
@@ -14,6 +15,11 @@ namespace Hackney.Shared.Asset.Domain
         public int NumberOfShowerRooms { get; set; }
         public int NumberOfBathrooms { get; set; }
         public string AssetPropertyFolderLink { get; set; }
+        public int BathroomType { get; set; }
+        public int KitchenType { get; set; }
+        public bool StepFreeAccess { get; set; }
+        public bool RampAccess { get; set; }
+        public List<string> SupplierInformation { get; set; }
         public DateTime? EpcExpiryDate { get; set; }
         public DateTime? FireSafetyCertificateExpiryDate { get; set; }
         public DateTime? GasSafetyCertificateExpiryDate { get; set; }
