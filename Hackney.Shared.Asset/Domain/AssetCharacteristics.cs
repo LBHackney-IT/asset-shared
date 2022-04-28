6590d14cad4f321fbd1e393269297f140183a75b
venkatrangala <venkatrangala@gmail.com>
2022-04-28 14:19:49 +0100

    Changes to Add & modify properties for Temporary Accommodation

--- Hackney.Shared.Asset/Domain/AssetCharacteristics.cs
+++ Hackney.Shared.Asset/Domain/AssetCharacteristics.cs
@@ -19,16 +19,12 @@ namespace Hackney.Shared.Asset.Domain
         public bool OptionToTax { get; set; }
         public bool HasStairs { get; set; }
         public int NumberOfStairs { get; set; }
-        public bool HasLift { get; set; }
         public bool RampAccess { get; set; }
         public bool CommunalAreas { get; set; }
-        public bool HasBathroom { get; set; }
         public bool PrivateBathroom { get; set; }
         public int NumberOfBathrooms { get; set; }
         public string BathroomFloor { get; set; }
-        public bool HasKitchen { get; set; }
         public bool PrivateKitchen { get; set; }
-        public bool GroundFloor { get; set; }
         public int NumberOfKitchens { get; set; }
         public string Kitchenfloor { get; set; }
         public DateTime AlertSystemExpiryDate { get; set; }
@@ -43,6 +39,9 @@ namespace Hackney.Shared.Asset.Domain
         public int KitchenType { get; set; }
         public bool StepFree { get; set; }
         public List<string> SupplierInformation { get; set; }
+        public bool IsStepFree { get; set; }
+        public bool HasPrivateBathroom { get; set; }
+        public bool HasPrivateKitchen { get; set; }
     }
 }
 
--- Hackney.Shared.Asset/Domain/AssetManagement.cs
+++ Hackney.Shared.Asset/Domain/AssetManagement.cs
@@ -13,36 +13,14 @@ namespace Hackney.Shared.Asset.Domain
         public bool IsTMOManaged { get; set; }
         public string PropertyOccupiedStatus { get; set; }
         public bool IsNoRepairsMaintenance { get; set; }
-        public string SupplierName { get; set; }
-        public string SupplierEmail { get; set; }
-        public string SupplierDetails { get; set; }
-        public string PatchOfficer { get; set; }
-        public string LandlordName { get; set; }
-        public string LandlordEmail { get; set; }
-        public string LandlordDetails { get; set; }
-        public string HostelManagerName { get; set; }
-        public string HostelManagerEmail { get; set; }
-        public string HostelManagerDetails { get; set; }
-        public string HostelCaretakerName { get; set; }
-        public string HostelCaretakerEmail { get; set; }
-        public string HostelCaretakerDetails { get; set; }
-        public DateTime ContractRenewDate { get; set; }
         public string FundingSource { get; set; }
         public string CostCentre { get; set; }
-        public string CouncilTaxBand { get; set; }
         public string CouncilTaxType { get; set; }
         public string CouncilTaxLiability { get; set; }
-        public double RatesNightly { get; set; }
-        public double RatesWeekly { get; set; }
-        public double RatesMonthly { get; set; }
         public string LhaArea { get; set; }
-        public double LhaRate { get; set; }
-        public AssetType AssetStatus { get; set; }
-        public bool AvailableToTA { get; set; }
+        public bool IsTemporaryAccomodation { get; set; }
         public bool ReadyToLetDate { get; set; }
-        public bool IsHackneyManaged { get; set; }
-        public bool StepFree { get; set; }
-        public bool PrivateBathroom { get; set; }
-        public bool PrivateKitchen { get; set; }
+        public AssetType AssetStatus { get; set; }
+        public bool OptionToTax { get; set; }
     }
 }
