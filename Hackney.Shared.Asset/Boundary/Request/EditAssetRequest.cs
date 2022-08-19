d486953755b1a5abb78b007ebbabf85e5daf3b03
huss@anefiox.com <huss@anefiox.com>
2022-08-19 13:52:49 +0100

    EditAssetRequest added

---
+++ Hackney.Shared.Asset/Boundary/Request/EditAssetRequest.cs
@@ -0,0 +1,25 @@
+﻿using Hackney.Shared.Asset.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hackney.Shared.Asset.Boundary.Request
+{
+    public class EditAssetRequest
+    {
+        public AssetType AssetType { get; set; }
+
+        public string RootAsset { get; set; }
+
+        public string ParentAssetIds { get; set; }
+
+        public AssetLocation AssetLocation { get; set; }
+
+        public AssetAddress AssetAddress { get; set; }
+
+        public AssetManagement AssetManagement { get; set; }
+
+        public AssetCharacteristics AssetCharacteristics { get; set; }
+
+    }
+}
