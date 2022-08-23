202b60c1a1bfbabd38e4951c136659ffacd571ef
huss@anefiox.com <huss@anefiox.com>
2022-08-23 09:26:33 +0100

    Edit asset request with asset type and address removed

--- Hackney.Shared.Asset/Boundary/Request/EditAssetRequest.cs
+++ Hackney.Shared.Asset/Boundary/Request/EditAssetRequest.cs
@@ -7,15 +7,11 @@ namespace Hackney.Shared.Asset.Boundary.Request
 {
     public class EditAssetRequest
     {
-        public AssetType AssetType { get; set; }
-
         public string RootAsset { get; set; }
 
         public string ParentAssetIds { get; set; }
 
         public AssetLocation AssetLocation { get; set; }
-
-        public AssetAddress AssetAddress { get; set; }
 
         public AssetManagement AssetManagement { get; set; }
 
