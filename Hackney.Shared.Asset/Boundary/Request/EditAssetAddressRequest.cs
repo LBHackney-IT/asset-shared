7fdfc2f4d99f4e46735dd091f40d7e40cce9b67a
Dave Dean <david.dean@hackney.gov.uk>
2023-02-23 13:33:03 +0000

    Add EditAssetAddressRequest

---
+++ Hackney.Shared.Asset/Boundary/Request/EditAssetAddressRequest.cs
@@ -0,0 +1,9 @@
+﻿using Hackney.Shared.Asset.Domain;
+
+namespace Hackney.Shared.Asset.Boundary.Request
+{
+    public class EditAssetAddressRequest : EditAssetRequest
+    {
+        public AssetAddress AssetAddress { get; set; }
+    }
+}
