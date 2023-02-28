2220add674a210c13bea4d7d5b6dfe74ceef01e6
Dave Dean <david.dean@hackney.gov.uk>
2023-02-28 17:40:27 +0000

    Added validator

---
+++ Hackney.Shared.Asset/Boundary/Request/Validation/EditAssetAddressRequestValidator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+using Hackney.Core.Validation;
+
+namespace Hackney.Shared.Asset.Boundary.Request.Validation
+{
+    public class EditAssetAddressRequestValidator : AbstractValidator<EditAssetAddressRequest>
+    {
+        public EditAssetAddressRequestValidator()
+        {
+            RuleFor(x => x.AssetAddress).NotNull()
+                                        .NotEmpty()
+                                        .WithErrorCode(ErrorCodes.AssetAddressEmptyOrInvalid);
+
+            RuleFor(x => x.AssetAddress).NotXssString()
+                                        .WithErrorCode(ErrorCodes.XssFailure);
+
+            RuleFor(x => x.AssetAddress.Uprn).NotNull()
+                                             .NotEmpty()
+                                             .WithErrorCode(ErrorCodes.AssetAddressUprnEmptyOrInvalid);
+
+            RuleFor(x => x.AssetAddress.Uprn).NotXssString()
+                                             .WithErrorCode(ErrorCodes.XssFailure);
+
+            RuleFor(x => x.AssetAddress.AddressLine1).NotNull()
+                                                     .NotEmpty()
+                                                     .WithErrorCode(ErrorCodes.AssetAddressLine1EmptyOrInvalid);
+
+            RuleFor(x => x.AssetAddress.AddressLine1).NotXssString()
+                                                     .WithErrorCode(ErrorCodes.XssFailure);
+
+            RuleFor(x => x.AssetAddress.AddressLine2).NotXssString()
+                                         .WithErrorCode(ErrorCodes.XssFailure);
+            RuleFor(x => x.AssetAddress.AddressLine3).NotXssString()
+                                         .WithErrorCode(ErrorCodes.XssFailure);
+            RuleFor(x => x.AssetAddress.AddressLine4).NotXssString()
+                                         .WithErrorCode(ErrorCodes.XssFailure);
+
+            RuleFor(x => x.AssetAddress.PostCode).NotNull()
+                                                 .NotEmpty()
+                                                 .WithErrorCode(ErrorCodes.AssetAddressPostcodeEmptyOrInvalid);
+
+            RuleFor(x => x.AssetAddress.PostCode).NotXssString()
+                                                 .WithErrorCode(ErrorCodes.XssFailure);
+        }
+    }
+}
