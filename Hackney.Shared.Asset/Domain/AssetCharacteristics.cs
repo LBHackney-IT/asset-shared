a6a8e515f0891b11a38db2062c2e0babca803981
ange47rm <ange47rm@hotmail.com>
2023-04-24 10:42:46 +0100

    Removed nullable types from AssetCharacteristics (introduced in 0.27)

--- Hackney.Shared.Asset/Domain/AssetCharacteristics.cs
+++ Hackney.Shared.Asset/Domain/AssetCharacteristics.cs
@@ -4,9 +4,9 @@ namespace Hackney.Shared.Asset.Domain
 {
     public class AssetCharacteristics
     {
-        public int? NumberOfBedrooms { get; set; }
-        public int? NumberOfLifts { get; set; }
-        public int? NumberOfLivingRooms { get; set; }
+        public int NumberOfBedrooms { get; set; }
+        public int NumberOfLifts { get; set; }
+        public int NumberOfLivingRooms { get; set; }
         public string WindowType { get; set; }
         public string YearConstructed { get; set; }
         public string AssetPropertyFolderLink { get; set; }
@@ -14,26 +14,26 @@ namespace Hackney.Shared.Asset.Domain
         public DateTime? FireSafetyCertificateExpiryDate { get; set; }
         public DateTime? GasSafetyCertificateExpiryDate { get; set; }
         public DateTime? ElecCertificateExpiryDate { get; set; }
-        public bool? HasStairs { get; set; }
-        public int? NumberOfStairs { get; set; }
-        public bool? HasRampAccess { get; set; }
-        public bool? HasCommunalAreas { get; set; }
-        public bool? HasPrivateBathroom { get; set; }
-        public int? NumberOfBathrooms { get; set; }
+        public bool HasStairs { get; set; }
+        public int NumberOfStairs { get; set; }
+        public bool HasRampAccess { get; set; }
+        public bool HasCommunalAreas { get; set; }
+        public bool HasPrivateBathroom { get; set; }
+        public int NumberOfBathrooms { get; set; }
         public string BathroomFloor { get; set; }
-        public bool? HasPrivateKitchen { get; set; }
-        public int? NumberOfKitchens { get; set; }
+        public bool HasPrivateKitchen { get; set; }
+        public int NumberOfKitchens { get; set; }
         public string Kitchenfloor { get; set; }
         public DateTime? AlertSystemExpiryDate { get; set; }
         public string EpcScore { get; set; }
-        public int? NumberOfFloors { get; set; }
+        public int NumberOfFloors { get; set; }
         public string AccessibilityComments { get; set; }
-        public int? NumberOfBedSpaces { get; set; }
-        public int? NumberOfCots { get; set; }
+        public int NumberOfBedSpaces { get; set; }
+        public int NumberOfCots { get; set; }
         public string SleepingArrangementNotes { get; set; }
-        public int? NumberOfShowers { get; set; }
+        public int NumberOfShowers { get; set; }
         public string KitchenNotes { get; set; }
-        public bool? IsStepFree { get; set; }
+        public bool IsStepFree { get; set; }
         public string BathroomNotes { get; set; }
         public string LivingRoomNotes { get; set; }
     }
