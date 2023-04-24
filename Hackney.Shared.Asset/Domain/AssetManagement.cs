75f0396e65a5feea7aee82333fd0b78a69d074cf
ange47rm <ange47rm@hotmail.com>
2023-04-24 10:44:24 +0100

    Removed nullable types from AssetManagement and AssetLocation (introduced in version 0.27)

--- Hackney.Shared.Asset/Domain/AssetManagement.cs
+++ Hackney.Shared.Asset/Domain/AssetManagement.cs
@@ -13,10 +13,10 @@ namespace Hackney.Shared.Asset.Domain
         public bool IsTMOManaged { get; set; }
         public string PropertyOccupiedStatus { get; set; }
         public string PropertyOccupiedStatusReason { get; set; }
-        public bool? IsNoRepairsMaintenance { get; set; }
+        public bool IsNoRepairsMaintenance { get; set; }
         public string CouncilTaxType { get; set; }
         public string CouncilTaxLiability { get; set; }
-        public bool? IsTemporaryAccomodation { get; set; }
-        public bool? ReadyToLetDate { get; set; }
+        public bool IsTemporaryAccomodation { get; set; }
+        public bool ReadyToLetDate { get; set; }
     }
 }
