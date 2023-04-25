9a8b4eeb8faec7fd581d82cfe37ea04197ee1410
Dave Dean <david.dean@hackney.gov.uk>
2023-04-25 18:32:46 +0100

    Revert "Undo changes introduced by version 0.27 (nullable types removed from Asset) (#31)"
    This reverts commit 07fbce82aece22a0f8cbefde9ad14a404bded2ae.

--- Hackney.Shared.Asset/Domain/AssetLocation.cs
+++ Hackney.Shared.Asset/Domain/AssetLocation.cs
@@ -5,7 +5,7 @@ namespace Hackney.Shared.Asset.Domain
     public class AssetLocation
     {
         public string FloorNo { get; set; }
-        public int TotalBlockFloors { get; set; }
+        public int? TotalBlockFloors { get; set; }
         public IEnumerable<ParentAsset> ParentAssets { get; set; }
     }
 }
