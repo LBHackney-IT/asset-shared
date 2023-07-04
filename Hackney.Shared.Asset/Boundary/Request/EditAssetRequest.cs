c9f9900132e26d7d145dac759b2b36173187fdc5
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2023-07-04 12:10:34 +0100

    Add missed boilerHouse property

--- Hackney.Shared.Asset/Boundary/Request/EditAssetRequest.cs
+++ Hackney.Shared.Asset/Boundary/Request/EditAssetRequest.cs
@@ -8,15 +8,11 @@ namespace Hackney.Shared.Asset.Boundary.Request
     public class EditAssetRequest
     {
         public string RootAsset { get; set; }
-
         public string ParentAssetIds { get; set; }
-
+        public string BoilerHouseId { get; set; }
         public bool IsActive { get; set; }
         public AssetLocation AssetLocation { get; set; }
-
         public AssetManagement AssetManagement { get; set; }
-
         public AssetCharacteristics AssetCharacteristics { get; set; }
-
     }
 }
