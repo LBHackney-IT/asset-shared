e77925eaa77e4195b81acc512bbe17d9d0fe6c6f
humulla <43747248+humulla@users.noreply.github.com>
2025-03-28 10:36:14 +0000

    add dependency to editPropertyPatchRequest

--- Hackney.Shared.Asset/Boundary/Request/EditPropertyPatchRequest.cs
+++ Hackney.Shared.Asset/Boundary/Request/EditPropertyPatchRequest.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Hackney.Shared.Asset.Boundary.Request
 {
-    public class EditPropertyPatchRequest
+    public class EditPropertyPatchRequest : EditAssetRequest
     {
         public Guid AreaId { get; set; }
         public Guid PatchId { get; set; }
